a9caac7af4e829be47090eac2d1cea9137d71de5
chu <chu295@qq.com>
2021-09-01 14:56:57 +0800

    完善深拷贝

--- handwritten/deepClone.js
+++ handwritten/deepClone.js
@@ -2,11 +2,13 @@ function deepClone(val) {
   if (typeof val !== "object") {
     return val;
   }
+  // 需要考虑正则时间戳，这里没写，可以补上正则 日期等等各种转换
   let newObj = Array.isArray(val) ? [] : {};
   for (const key in val) {
     let item = val[key];
     newObj[key] = item instanceof Object ? deepClone(item) : item;
   }
+  return newObj
 }
 
 var a = {
