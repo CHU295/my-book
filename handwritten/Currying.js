32969e4e82eccc02a50c241efbe4403f84dca976
chu <chu295@qq.com>
2021-09-02 09:58:19 +0800

    柯里化

--- handwritten/Currying.js
+++ handwritten/Currying.js
@@ -1,3 +1,26 @@
+function sum(a, b, c) {
+  return a + b + c;
+}
+sum(1, 2, 3);
+
+function currying(fuc) {
+  let new_ = (...arg) => {
+    if (fuc.length === arg.length) {
+      return fuc(...arg);
+    } else {
+      return (...args) => new_(...arg, ...args);
+    }
+  };
+  return new_;
+}
+const newSum = currying(sum);
+console.log(newSum(1)(2)(3))
+/**
+ * 知识点备注
+ * function 有 length 属性，表示的是必须入参的长度，参考mdn
+ */
+
+
 // add(1)(2, 3)(4)() = 10
 function add(...arg) {
   let count = arg.reduce((pre, cur) => pre + cur);
